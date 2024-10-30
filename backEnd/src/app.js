1ca53810294356d399bae6b3c015c1ecb37f6b82
harshsharma7102001 <harshsharma7102001@gmail.com>
2024-10-30 22:38:42 +0530

    used regex and multiple command handlers

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -1,18 +1,61 @@
 const express = require('express');
-
+const { deserialize } = require('mongodb');
 const app = express();
 
+var data = [{
+    firstName :"Harsh Raj",
+    lastName:"Raj",
+    designation:"Jr Software Dev"
+},
+{
+    firstName:"John",
+    lastName:"Ciena",
+    designation:"Wrestler"
+}
+]
 
-app.use("/check",(req,res)=>{
-    res.send("Hello there");
-});
+app.get("/user/:id/:name/:add",(req,res)=>{
+    console.log(req.params);
+    res.send("Got it");
+})
 
-app.use("/test",(req,res)=>{
-    res.send("Homework cheking test");
+//-> Checked multiple handlers in same api
+app.get("/profile",
+    (req,res,next)=>{
+        next();
+        //res.send("Data send successfully!");
+    },
+    (req,res)=>{
+        console.log("Entered second handler");
+        res.send("This is second one");
+    }
+)
+//Using Regular Expressions
+app.get("/pro*file",(req,res)=>{
+    res.send(data)
 })
-app.use((req,res)=>{
-    res.send("Dude i am listening now");
+
+app.post("/profile",(req,res)=>{
+    console.log(req);
+    data[0].firstName = req.query.firstName,
+    data[0].lastName = req.query.lastName,
+    data[0].designation = req.query.designation
+    res.send("Data updated successfully");
 })
+
+app.put("/profile",(req,res)=>{
+    data[0].designation = req.query.designation
+    res.send("Data upadated Successfully");
+})
+
+app.delete("/profile",(req,res)=>{
+    data.shift();
+    res.send("Data deleted successfully");
+})
+app.use("/testUrl",(req,res)=>{
+    res.send("Just testing my knowledge");
+})
+
 app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+    console.timeLog("Server started at port 3000");
 })
